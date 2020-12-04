080bb3498a23dc1b08064c10f29e556227681d33
adelaida-barrera <71333816+adelaida-barrera@users.noreply.github.com>
2020-12-04 22:11:36 +0100

    Add files via upload

---
+++ R/balance_tbl.R
@@ -0,0 +1,27 @@
+
+#' Create a balance table usually needed for the course Statistics II at the Hertie School 
+#'
+#' Creates a well-formatted table containing the mean values of a set of covariates for treatment and control groups. 
+#' 
+#'
+#' @param data a dataframe containing only a binary treatment variable and a series of covariates to be included in the table 
+#' @param treatment the treatment variable in the provided dataframe
+#'
+#' @return table
+#' @export
+
+balance_tbl <- function (data, treatment) {
+  library(tidyverse)
+  library(knitr)
+  library(kableExtra)
+  data %>%
+    dplyr::group_by(treatment) %>%
+    dplyr::summarize_all(funs(mean(., na.rm = T))) %>% # summarize the mean of every variable
+    t() %>% # transpose data
+    as.data.frame() %>% # after transposing, convert from matrix to df
+    add_rownames("variable") %>% # rownames to explicit column
+    dplyr::rename(control = V1, treat = V2) %>% # rename columns that are created by as.data.frame
+    dplyr::filter(variable != "treat" & variable != "data_id") %>% 
+    knitr::kable(col.names = c("Variable", "Control", "Treat", "P value")) %>% # create kable table
+    kableExtra::kable_styling()# drop redundant term  
+}
